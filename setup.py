923295eb47dc43c24e784aa797425c827de85455
Oriol Vallcorba <ovallcorba@cells.es>
2023-11-17 13:52:33 +0100

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sardana-redis",
-    version="0.1.0",
+    version="0.2.0",
     description="Sardana BlissData 1.0 Redis Recorder",
     author="Oriol Vallcorba",
     author_email="ovallcorba@cells.es",
